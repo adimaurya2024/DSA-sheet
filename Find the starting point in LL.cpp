b65482b800a15b3016cc973b316eb0a8087c9e8b
Aditya Maurya <115164737+adimaurya2024@users.noreply.github.com>
2023-10-03 23:53:02 +0530

    Create Find the starting point in LL.cpp

---
+++ Find the starting point in LL.cpp
@@ -0,0 +1,27 @@
+lass Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+       if(head==NULL || head->next==NULL)
+       {
+           return NULL;
+       } 
+       ListNode* slow= head;
+       ListNode* fast= head;
+       ListNode * entry = head;
+       while(fast!= NULL && fast->next!= NULL)
+       {
+           slow= slow->next;
+           fast= fast->next->next;
+           if(slow==fast)
+           {
+               while(entry!= slow)
+               {
+                   entry= entry->next;
+                   slow= slow->next;
+               }
+               return entry;
+           }
+       }
+       return NULL;
+    }
+};
